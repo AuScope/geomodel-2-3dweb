3e0592f597c8674cf2742ac6b1ee2f4ae910dbd5
Vincent Fazio <vjf@users.noreply.github.com>
2019-12-04 09:33:31 +1100

    AUS-3150 Converts well coords to model geometry and metadata types

--- scripts/lib/imports/gocad/gocad_importer.py
+++ scripts/lib/imports/gocad/gocad_importer.py
@@ -493,7 +493,16 @@ class GocadImporter():
                     # All well files
                     if field[0] == "PATH_ZM_UNIT" or field[0] == "WREF":
                         self.logger.debug("Processing ASCII well path")
-                        is_last, well_path, marker_list = self.process_ascii_well_path(line_gen, field)
+                        is_last, well_path, self.meta_obj.marker_list = self.process_ascii_well_path(line_gen, field)
+                        # Convert well path into a series of SEG types
+                        if len(well_path) > 1:
+                            self.__vrtx_arr.append(VRTX(1, well_path[0]))
+                            for idx in range(1,len(well_path)):
+                                self.__seg_arr.append(SEG((idx, idx+1)))
+                                self.__vrtx_arr.append(VRTX(idx+1, well_path[idx]))
+                             
+                        self.logger.debug("Well path: %s", repr(well_path))
+                        self.logger.debug("Marker list: %s", repr(self.meta_obj.marker_list))
                         retry = True
 
                     # Well files with well curve block
